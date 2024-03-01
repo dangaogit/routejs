5543b6fcd3ac5ef4b7595cb6f86e5c9bfc510416
dangaogit <dangaogm@gmail.com>
2024-03-01 11:01:27 +0800

    {init, core}: init repo && impl uri-matcher

---
+++ src/core/uri-matcher/uri-matcher.test.ts
@@ -0,0 +1,51 @@
+import { URIMatcher } from './uri-matcher'
+import { assert, describe, test } from 'vitest'
+
+describe('uri-matcher', () => {
+  test('match normal uri', () => {
+    const pathConfig = '/path/:par1/path2/:par2'
+    const targetURI = '/path/1/path2/2?par3=3&par4=4'
+    const underTest = new URIMatcher(pathConfig)
+    assert.equal(underTest.test(targetURI), true)
+    assert.equal(underTest.test(targetURI), true)
+  })
+  test('not match uri', () => {
+    const pathConfig = '/path/:par1/path2/:par2'
+    const targetURI = '/path/1/path2'
+    const underTest = new URIMatcher(pathConfig)
+    assert.equal(underTest.test(targetURI), false)
+  })
+  test('get path params', () => {
+    const pathConfig = '/path/:par1/path2/:par2'
+    const targetURI = '/path/1/path2/2?par3=3&par4=4'
+    const underTest = new URIMatcher(pathConfig).match(targetURI).getPathParams()
+    assert.equal(underTest['par1'], '1')
+    assert.equal(underTest['par2'], '2')
+  })
+  test('get path params safety', () => {
+    const pathConfig = '/path/:par1/path2/:par2'
+    const targetURI = '/path/1/path2/2?par3=3&par4=4'
+    const uriMatchResult = new URIMatcher(pathConfig).match(targetURI)
+    const underTest = uriMatchResult.getPathParams()
+    assert.equal(underTest['par1'], '1')
+    underTest.par1 = '2'
+    assert.equal(uriMatchResult.getPathParams()['par1'], '1')
+  })
+  test('get query params', () => {
+    const pathConfig = '/path/:par1/path2/:par2'
+    const targetURI = '/path/1/path2/2?par3=3&par4=4'
+    const underTest = new URIMatcher(pathConfig).match(targetURI).getQueryParams()
+    assert.equal(underTest['par3'], '3')
+    assert.equal(underTest['par4'], '4')
+  })
+  test('get query params safety', () => {
+    const pathConfig = '/path/:par1/path2/:par2'
+    const targetURI = '/path/1/path2/2?par3=3&par4=4'
+    const uriMatchResult = new URIMatcher(pathConfig).match(targetURI)
+    const underTest = uriMatchResult.getQueryParams()
+    assert.equal(underTest['par3'], '3')
+    underTest.par3 = '100'
+    assert.equal(uriMatchResult.getQueryParams()['par3'], '3')
+  })
+})
+
---
+++ src/core/uri-matcher/uri-matcher.ts
@@ -0,0 +1,61 @@
+class URIMatchResult {
+  private readonly pathParams: Record<string, string> = {}
+  private readonly queryParams: Record<string, string> = {}
+
+  public constructor(pathConfig: string, targetURI: string) {
+    const [ pathname, queryString ] = targetURI.split('?')
+    this.pathParams = analysisURIPathParams(pathConfig, pathname)
+    this.queryParams = analysisURIQueryParams(queryString)
+  }
+
+  public getPathParams(): Record<string, string> {
+    return {
+      ...this.pathParams
+    }
+  }
+
+  public getQueryParams(): Record<string, string> {
+    return {
+      ...this.queryParams
+    }
+  }
+}
+
+export class URIMatcher {
+  public constructor(private readonly pathConfig: string) {
+  }
+
+  public test(targetURI: string): boolean {
+    const reg = analysisPathConfigToRegExp(this.pathConfig, true)
+    return reg.test(targetURI)
+  }
+
+  public match(targetURI: string): URIMatchResult {
+    if (!this.test(targetURI)) {
+      throw new Error('URI not match')
+    }
+    return new URIMatchResult(this.pathConfig, targetURI)
+  }
+}
+
+function analysisURIPathParams(pathConfig: string, targetURI: string): Record<string, string> {
+  const reg = analysisPathConfigToRegExp(pathConfig, true)
+  return {
+    ...targetURI.match(reg)?.groups
+  }
+}
+
+function analysisPathConfigToRegExp(path: string, absoluteMatch = false): RegExp {
+  return new RegExp(`^${ path }/?${ absoluteMatch ? '$' : '' }`.replace(/\/:([^\\/]+)/g, (match) => {
+    return `\\/(?<${ match.slice(2) }>[^\\/]+)`
+  }))
+}
+
+function analysisURIQueryParams(queryString: string): Record<string, string> {
+  const searchParams = new URLSearchParams(queryString)
+  const result: Record<string, string> = {}
+  for (const [ key, value ] of searchParams) {
+    result[key] = value
+  }
+  return result
+}
